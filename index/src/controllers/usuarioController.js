23f9a167592c8e3b5ce501c0208b809499ff8652
JoaoVtC <joao.varaujo@sptech.school>
2025-05-14 16:03:00 -0300

    Alteração no model e controler

--- index/src/controllers/usuarioController.js
+++ index/src/controllers/usuarioController.js
@@ -53,14 +53,13 @@ function autenticar(req, res) {
 
 function cadastrar(req, res) {
     // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
-    var nome = req.body.nomeServer;
+    var nick = req.body.nickServer;
     var email = req.body.emailServer;
-    var senha = req.body.senhaServer;
-    var fkEmpresa = req.body.idEmpresaVincularServer;
+    var pwd = req.body.pwdServer;
 
     // Faça as validações dos valores
-    if (nome == undefined) {
-        res.status(400).send("Seu nome está undefined!");
+    if (nick == undefined) {
+        res.status(400).send("Seu nick está undefined!");
     } else if (email == undefined) {
         res.status(400).send("Seu email está undefined!");
     } else if (senha == undefined) {
@@ -70,7 +69,7 @@ function cadastrar(req, res) {
     } else {
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha, fkEmpresa)
+        usuarioModel.cadastrar(nick, email, pwd)
             .then(
                 function (resultado) {
                     res.json(resultado);
