ad459f58573347b26409744ecb67da8237a522a3
JoaoVtC <joao.varaujo@sptech.school>
2025-05-23 09:39:53 -0300

    login redirecionando pra tela de quiz

--- index/src/controllers/usuarioController.js
+++ index/src/controllers/usuarioController.js
@@ -19,19 +19,13 @@ function autenticar(req, res) {
                     if (resultadoAutenticar.length == 1) {
                         console.log(resultadoAutenticar);
 
-                        /*aquarioModel.buscarAquariosPorEmpresa(resultadoAutenticar[0].empresaId)
-                            .then((resultadoAquarios) => {
-                                if (resultadoAquarios.length > 0) {
-                                    res.json({
-                                        id: resultadoAutenticar[0].id,
-                                        email: resultadoAutenticar[0].email,
-                                        nick: resultadoAutenticar[0].nick,
-                                        pwd: resultadoAutenticar[0].pwd
-                                    });
-                                } else {
-                                    res.status(204).json({ aquarios: [] });
-                                }
-                            })*/
+                        res.json({
+                            id: resultadoAutenticar[0].idUsuario,
+                            nick: resultadoAutenticar[0].nickanme,
+                            email: resultadoAutenticar[0].email,
+                            senha: resultadoAutenticar[0].senha
+                        });
+
                     } else if (resultadoAutenticar.length == 0) {
                         res.status(403).send("Email e/ou pwd inválido(s)");
                     } else {
@@ -62,7 +56,7 @@ function cadastrar(req, res) {
         res.status(400).send("Seu email está undefined!");
     } else if (pwd == undefined) {
         res.status(400).send("Sua pwd está undefined!");
-    } 
+    }
     else {
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
