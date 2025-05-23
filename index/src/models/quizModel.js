b612aa491cf9a16ef483ec5cb082cd1b285db3ce
JoaoVtC <joao.varaujo@sptech.school>
2025-05-23 18:18:41 -0300

    alteração no model

--- index/src/models/quizModel.js
+++ index/src/models/quizModel.js
@@ -11,12 +11,14 @@ function autenticar(email, pwd) {
 
 // Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
 function pontuacao(idUsuario, tntv1, tntv2, tntv3, tntv4, tntv5,
-            naoRespondido, seqAcertos, errosTotais) {
+    naoRespondido, seqAcertos, errosTotais) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():");
-    
+
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucaoSql = `
+        INSERT INTO pontuacaoTotal (fkUsuario, acertosTntv1, acertosTntv2, acertosTntv3, acertosTntv4, acertosTntv5,
+            naoRespondido, errosTotais, seqAcertos) VALUES (${idUsuario}, ${tntv1}, ${tntv2}, ${tntv3}, ${tntv4}, ${tntv5}, ${naoRespondido}, ${errosTotais}, ${seqAcertos});
         INSERT INTO pontuacaoTotal (fkUsuario, acertosTntv1, acertosTntv2, acertosTntv3, acertosTntv4, acertosTntv5,
             naoRespondido, errosTotais, seqAcertos) VALUES (${idUsuario}, ${tntv1}, ${tntv2}, ${tntv3}, ${tntv4}, ${tntv5}, ${naoRespondido}, ${errosTotais}, ${seqAcertos});
     `;
